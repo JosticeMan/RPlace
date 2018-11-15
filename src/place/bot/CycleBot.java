1216fde57a5d542169e92179c77a72e164a215a1
Justin Yau <justiceman8998@gmail.com>
2018-11-14 23:15:15 -0500

    Created a bot that colors random tiles

--- src/place/bot/CycleBot.java
+++ src/place/bot/CycleBot.java
@@ -13,28 +13,22 @@ import java.io.IOException;
  * CSCI-242 AP COMPUTER SCIENCE X
  * Project 2: Place
  *
- *  Bot that spams the living heck out of the server with the same change tile request to make it theirs
+ *  Bot that spams the living heck out of the server with random change tile request to make it theirs
  *
  * @author Justin Yau
  */
-public class ProtectBot extends Bot {
-
-    private PlaceTile myTile; // The tile that the Bot will protect
+public class CycleBot extends Bot {
 
     /***
-     * Picks a tile if it hasn't already and creates a change tile request
+     * Picks a random tile and creates a change tile request for it
      * @param arg - Object that has been passed through notifyObservers(), if any.
      */
     @Override
     public void makeMove(Object arg) {
         ClientModel model = super.getModel();
         NetworkClient serverConn = super.getConn();
-        if(myTile == null) {
-            pickTile(model);
-        }
         try {
-            myTile.setTime(System.currentTimeMillis());
-            serverConn.createTileChangeRequest(myTile);
+            serverConn.createTileChangeRequest(pickTile(model));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,12 +38,12 @@ public class ProtectBot extends Bot {
      * Picks a random tile from the model and saves it to make it theirs
      * @param model - The model from the server
      */
-    private void pickTile(ClientModel model) {
+    private PlaceTile pickTile(ClientModel model) {
         int dim = model.getDim();
         int row = Bot.randomWithRange(0, dim);
         int col = Bot.randomWithRange(0, dim);
         int currentColor = Bot.randomWithRange(0, PlaceColor.TOTAL_COLORS - 1);
-        this.myTile = new PlaceTile(row, col, super.getUserName(), PlaceExchange.colors[currentColor],
+        return new PlaceTile(row, col, super.getUserName(), PlaceExchange.colors[currentColor],
                 System.currentTimeMillis());
     }
 
@@ -57,7 +51,7 @@ public class ProtectBot extends Bot {
         if (args.length != 3) {
             System.out.println("Usage: java PlaceClient host port username");
         } else {
-            ConsoleApplication.launch(ProtectBot.class, args);
+            ConsoleApplication.launch(CycleBot.class, args);
         }
     }
 }
